49acd36b17f7da900865520219df8e2267e9823c
Felipe Souza <felipedb91@gmail.com>
2020-07-21 20:56:24 -0300

    Refactory index method on TransactionController for add a description filter on database query

--- src/controllers/transaction-controller.js
+++ src/controllers/transaction-controller.js
@@ -3,13 +3,16 @@ import validator from 'express-validator';
 
 const TransactionController = {
   index: async (req, res) => {
-    const { period } = req.query;
+    const { period, description = '' } = req.query;
     if (!period) {
       return res.status(400).json({
         error: `É necessario informar o parametro "period", cujo o valor deve estar no formato yyyy-mm`,
       });
     }
-    const result = await Transactions.find({ yearMonth: period }).sort({
+    const result = await Transactions.find({
+      yearMonth: period,
+      description: { $regex: description, $options: 'i' },
+    }).sort({
       day: 'asc',
     });
 
